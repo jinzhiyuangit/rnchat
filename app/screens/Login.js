629c4b90a5827700bbf516f451c3e0995c3b85ff
jinzhiyuangit <jin.zhiyuan@hotmail.com>
2018-12-18 11:56:52 +0800

    login

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
 
 export default class Login extends React.Component {
   render() {
     return (
       <View>
-        <Text style={styles.label}>Username</Text>
+        <Image
+          style={styles.image}
+          source={require('../images/login.jpg')}
+        />
         <TextInput
           style={styles.text_field}
           onChangeText={this.props.updateUsername}
           value={this.props.username}
           placeholder="Enter your username"
         />
-
+        <TextInput
+          style={styles.text_field}
+          onChangeText={this.props.updatePassword}
+          value={this.props.password}
+          placeholder="Enter your password"
+        />
         <Button onPress={this.props.enterChat} title="Enter" color="#0064e1" />
       </View>
     );
@@ -34,4 +42,9 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
   },
+  image: {
+    width: 100,
+    height: 100,
+    alignSelf: 'center'
+  }
 });
